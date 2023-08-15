dcc01c8aa10a9cf37e05ad76eae62c7daf562c8f
Ratnasree Ghosh <86732136+ratnasree-ghosh@users.noreply.github.com>
2023-08-15 13:04:18 +0000

    3rd grid commit

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -12,12 +12,13 @@ import axios from "axios";
         axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
         .then((res)=> {
             setCoins(res.data);
-            console.log(res.data);
+            
         })
         .catch(e =>{
             console.log("Error", e);
         }, []);
     })
+    
 
     return <div className="dashboardPage"> 
         <Header/>
